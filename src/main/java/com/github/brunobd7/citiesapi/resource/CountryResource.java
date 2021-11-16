357d82d7b3c72e1aef8fa8af2217180b52592fcf
Bruno Dantas <brunoodantas7@gmail.com>
2021-11-16 18:24:22 -0300

    Project part 7 ok, pageable and show jpa executions

--- src/main/java/com/github/brunobd7/citiesapi/resource/CountryResource.java
+++ src/main/java/com/github/brunobd7/citiesapi/resource/CountryResource.java
@@ -1,8 +1,10 @@
-package com.github.brunobd7.citiesapi;
+package com.github.brunobd7.citiesapi.resource;
 
 import com.github.brunobd7.citiesapi.model.Country;
 import com.github.brunobd7.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,16 @@ import java.util.List;
 @RequestMapping("/countries")
 public class CountryResource {
 
-
     @Autowired
     CountryRepository countryRepository;
 
-    @GetMapping
+    /*@GetMapping
     public List<Country> listAll(){
         return countryRepository.findAll();
+    }*/
+
+    @GetMapping
+    public Page<Country> listAllPageable(Pageable pageable){
+        return countryRepository.findAll(pageable);
     }
 }
