69fca51dbb96a2ed9e6c7db813dced1719e4345c
Bruno Dantas <brunoodantas7@gmail.com>
2021-11-16 23:42:55 -0300

    Find by id jpa method implemented into country resource.

--- src/main/java/com/github/brunobd7/citiesapi/resource/CountryResource.java
+++ src/main/java/com/github/brunobd7/citiesapi/resource/CountryResource.java
@@ -3,13 +3,17 @@ package com.github.brunobd7.citiesapi.resource;
 import com.github.brunobd7.citiesapi.model.Country;
 import com.github.brunobd7.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/countries")
@@ -26,5 +30,15 @@ public class CountryResource {
     @GetMapping
     public Page<Country> listAllPageable(Pageable pageable){
         return countryRepository.findAll(pageable);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> findById(@PathVariable Long id){
+//        Country countryFounded = countryRepository.findById(id)
+//                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+        Country countryFounded = countryRepository.findById(id).orElse(null);
+
+        return Objects.nonNull(countryFounded) ? ResponseEntity.ok(countryFounded) : ResponseEntity.notFound().build();
     }
 }
